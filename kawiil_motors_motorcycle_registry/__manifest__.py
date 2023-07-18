a125a51d5be94770c987541ed60616a20697e710
Eduardo (levm) <levm@odoo.com>
2023-07-18 15:06:34 -0500

    [ADD] Creación nuevo módulo Kawiil

---
+++ kawiil_motors_motorcycle_registry/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name' : 'Motorcycle Registry',
+    'summary' : 'Manage Registration of Motorcycles',
+    'description' : """Motorcycle Registry
+====================
+This Module is used to keep track of the Motorcycle Registration and Ownership of each motorcycled of the brand.""",
+    'license' : 'OPL-1',
+    'author' : 'levm-odoo',
+    'website' : 'https://www.github.com/levm-odoo',
+    'category' : 'Kawiil',
+    'depends' : ['base'],
+    'data' : [],
+    'demo' : [],
+    'application' : True,
+}
